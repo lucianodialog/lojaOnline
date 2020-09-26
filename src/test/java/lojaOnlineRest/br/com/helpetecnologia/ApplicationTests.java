a6b380da48ded9e51acf41e698bd3d21aa21949c
Luciano <lucianodialgue@gmail.com/>
2020-09-26 03:32:42 +0000

    Commit Inicial com recurso basico de categoria

---
+++ src/test/java/lojaOnlineRest/br/com/helpetecnologia/ApplicationTests.java
@@ -0,0 +1,13 @@
+package lojaOnlineRest.br.com.helpetecnologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
