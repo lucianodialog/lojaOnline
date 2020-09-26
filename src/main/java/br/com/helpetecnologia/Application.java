79a584e54c7909489fcc0ffd67386ee0abc0d01e
Luciano <lucianodialgue@gmail.com/>
2020-09-26 13:50:16 +0000

    Crianto metodo de Intanciacao na classe Classe Aplication - Principal

--- src/main/java/br/com/helpetecnologia/Application.java
+++ src/main/java/br/com/helpetecnologia/Application.java
@@ -1,13 +1,36 @@
 package br.com.helpetecnologia;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.helpetecnologia.domain.Categoria;
+import br.com.helpetecnologia.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner{
+//A interface CommandLineRunner permite criar um método que é chamado quando a aplicação começa a rodar 	
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+@Override
+public void run(String... args) throws Exception {
+
+	Categoria cat1 = new Categoria(null, "Livros");
+	Categoria cat2 = new Categoria(null, "Vídeo");
+	
+	categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	
+	
+	
+}
 
 }
