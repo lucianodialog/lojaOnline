04d55a026322cf70be3e8ebd3b763be259b20133
Luciano <lucianodialgue@gmail.com/>
2020-09-26 03:47:26 +0000

    Testanto a primeira classe de dominio - categoria

--- src/main/java/br/com/helpetecnologia/resources/CategoriaResource.java
+++ src/main/java/br/com/helpetecnologia/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package br.com.helpetecnologia.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.helpetecnologia.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String ListarCategorias() {
-		return "Chamou categorias";
+	public List<Categoria> ListarCategorias() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");		
+		List<Categoria> lista_categorias = new ArrayList<>();
+		lista_categorias.add(cat1);
+		lista_categorias.add(cat2);
+		return lista_categorias;		
+		
+		//return "Chamou categorias";
 	}
 
 }
